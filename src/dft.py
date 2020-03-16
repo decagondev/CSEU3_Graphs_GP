051b8c10fd01d6d1474ecf9262e98955f95ea68b
Tom <tomtarpeydev@gmail.com>
2020-03-16 11:01:07 +0000

    end of day 1

--- src/dft.py
+++ src/dft.py
@@ -82,4 +82,20 @@ class Graph:
                     # push the next vertex
                     s.push(next_vertex)
 
+    def bfs(self, starting_vertex_id, target_vertex_id):
+        # Create an empty queue and enqueue A PATH TO the starting vertex ID
+        # Create a Set to store visited vertices
+        # While the queue is not empty...
+            # Dequeue the first PATH eg -> [a, b, c, r, g]
+            # Grab the last vertex from the PATH
+            # If that vertex has not been visited...
+                # CHECK IF IT'S THE TARGET
+                    # IF SO, RETURN PATH
+
+                # Mark it as visited...
+                # Then add A PATH TO its neighbors to the back of the queue
+                # COPY THE PATH
+                # APPEND THE NEIGHOR TO THE BACK
+        pass
+
     
